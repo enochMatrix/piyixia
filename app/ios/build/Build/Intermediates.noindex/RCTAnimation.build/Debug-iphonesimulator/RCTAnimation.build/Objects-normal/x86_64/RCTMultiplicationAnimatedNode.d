1b8d3f4e16b6341c92e2783c481e88b822e346e2
zhezhang91 <zz188659@gmail.com>
2018-05-07 17:54:42 -0400

    05-07 6pm learnt Touch event, Press event, ScrollView, FlatList, adding image

--- app/ios/build/Build/Intermediates.noindex/RCTAnimation.build/Debug-iphonesimulator/RCTAnimation.build/Objects-normal/x86_64/RCTMultiplicationAnimatedNode.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/majingyi/app/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTMultiplicationAnimatedNode.m \
-  /Users/majingyi/app/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTMultiplicationAnimatedNode.h \
-  /Users/majingyi/app/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTValueAnimatedNode.h \
-  /Users/majingyi/app/node_modules/react-native/Libraries/NativeAnimation/Nodes/RCTAnimatedNode.h
--- app/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTActivityIndicatorView.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/majingyi/app/node_modules/react-native/React/Views/RCTActivityIndicatorView.m \
-  /Users/majingyi/app/node_modules/react-native/React/Views/RCTActivityIndicatorView.h
