f658788c0accd475a814d883785590f175bfaed8
马静怡 <472382917@qq.com>
2018-05-07 18:59:30 -0400

    5.7: 1. seperate 3 components(InputPlace,List,ListItem )from App component. 2. adding touchable tag to operate the delete method by adding deleteHandler as a function in List Component 3.Using FlatList to scroll the screen when typingin too much items. thus, we can replace the original Map meyhod, the must properties in it are Date={} and renderItem={()=>()}. done!

--- app/src/components/List/List.js
+++ app/src/components/List/List.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {View,StyleSheet} from 'react-native';
+import {StyleSheet,FlatList} from 'react-native';
+import ListItem from '../ListItem/ListItem';
 
-const list=(props)=>(
+const list=(props)=> {
 
-    <View style={styles.listContainer}>
-        {props.placesOutput}
-    </View>
 
-);
+
+
+    return (
+
+            <FlatList
+                style={styles.listContainer}
+            data={props.places}
+            renderItem={(info)=>(
+                <ListItem
+                          placeName={info.item.value}
+                          onItemPressed={()=>props.onItemDeleted(info.item.key)}/>
+
+            )}/>
+            //no longer use map method on render
+
+
+    );
+}
+
+
 const styles = StyleSheet.create({
     listContainer:{
         width:"100%"
