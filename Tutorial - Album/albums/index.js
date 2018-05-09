f61365087461a0cc924db243a61ea193677fc3a2
GaoYuxin <gaomnemosyne@Gmail.com>
2018-05-08 22:49:32 -0400

    upload the whole album file

---
+++ Tutorial - Album/albums/index.js
@@ -0,0 +1,20 @@
+//import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
+
+/*
+create a component
+component nesting
+*/
+const App = () => ( // flex:1 is for scrollview property
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+  );
+
+
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
