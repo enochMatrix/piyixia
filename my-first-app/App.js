ab3dfb0d9ad03225ad3f9678e28db90a1ba0eb29
zhezhang91 <zz188659@gmail.com>
2018-05-12 12:48:18 -0400

    05-12 updated Redux to react native

--- my-first-app/App.js
+++ my-first-app/App.js
@@ -5,56 +5,62 @@ import UserInput from './src/components/UserInput/UserInput';
 import PlaceOutput from './src/components/PlaceOutput/PlaceOutput';
 import placeImage from './src/assets/cats.jpg'; // local image
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+import {connect} from 'react-redux';
+import {addPlace, deselectPlace, deletePlace, selectPlace} from './src/store/actions/index';
 
-export default class App extends React.Component {
-
-    state = {
-        places: [],
-        selectPlace: null
-    };
+class App extends React.Component {
+    // put in reduex store....
+    // state = {
+    //     places: [],
+    //     selectPlace: null
+    // };
 
 // 输入处理函数
 
     placeAddedHandler = (placeName) => {
         // concat all the inputs by user in a array, ['','','',...]
-        this.setState(prevState => {
-            return {
-                places: prevState.places.concat({
-                    key: Math.random(),
-                    name: placeName,
-                    image: {
-                        // fetch image from online
-                        uri: "https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
-                    }
-                })
-            }
-        })
+        // this.setState(prevState => {
+        //     return {
+        //         places: prevState.places.concat({
+        //             key: Math.random(),
+        //             name: placeName,
+        //             image: {
+        //                 // fetch image from online
+        //                 uri: "https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
+        //             }
+        //         })
+        //     }
+        // })
+        this.props.onAddPlace(placeName);
     };
 
     placeSelectedHandler = key => {
 
-        this.setState(prevState => {
-            return {
-                selectPlace: prevState.places.find(place => place.key === key)
-            }
-        })
+        // this.setState(prevState => {
+        //     return {
+        //         selectPlace: prevState.places.find(place => place.key === key)
+        //     }
+        // })
+        this.props.onSelectPlace(key);
     };
 
     placeDeletedHandler = () => {
-        this.setState(prevState => {
-            return {
-                places: prevState.places.filter( place => {
-                    return place.key !== prevState.selectPlace.key
-                }),
-                selectPlace: null
-            }
-        })
+        // this.setState(prevState => {
+        //     return {
+        //         places: prevState.places.filter( place => {
+        //             return place.key !== prevState.selectPlace.key
+        //         }),
+        //         selectPlace: null
+        //     }
+        // })
+        this.props.onDeletePlace();
     };
 
     modalClosedHandler = () => {
-        this.setState({
-            selectPlace: null
-        })
+        // this.setState({
+        //     selectPlace: null
+        // })
+        this.props.onDeSelectPlace();
     };
 
   render() {
@@ -67,12 +73,12 @@ export default class App extends React.Component {
       <View style={styles.container}>
           {/*文本输入框*/}
 
-          <PlaceDetail selectedPlace={this.state.selectPlace}
+          <PlaceDetail selectedPlace={this.props.selectPlace}
                        onItemDeleted={this.placeDeletedHandler}
                        onModalClosed={this.modalClosedHandler}/>
           <UserInput onPlaceAdded={this.placeAddedHandler}/>
           <PlaceOutput
-             places={this.state.places}
+             places={this.props.places}
              onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
@@ -108,3 +114,21 @@ const styles = StyleSheet.create({
       width: "100%"
     }
 });
+
+const mapStateToProps = state => {
+    return {
+        places: state.places.places,
+        selectPlace: state.places.selectPlace
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPlace: (name) => dispatch(addPlace(name)),
+        onSelectPlace: (key) => dispatch(selectPlace(key)),
+        onDeletePlace: () => dispatch(deletePlace()),
+        onDeSelectPlace: () => dispatch(deselectPlace()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- my-first-app/index.js
+++ my-first-app/index.js
@@ -1,3 +1,16 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('myfirstapp', () => App);
+import {Provider} from 'react-redux';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+
+AppRegistry.registerComponent('myfirstapp', () => RNRedux);
