4279a8068ea666754115913d6f1bcf9b09c519b0
zhezhang91 <zz188659@gmail.com>
2018-05-16 01:40:40 -0400

    0516 1:39AM Rebuild the app due to bugs

--- app/App.js
+++ app/App.js
@@ -1,64 +1,49 @@
-import React,{Component} from 'react';
-import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
-import ListItem from './src/components/ListItem/ListItem';
-import InputPlace from './src/components/InputPlace/InputPlace';
-import List from './src/components/List/List';
+import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
 
-export default class App extends Component {
-    state ={
-        placeName:'',
-        places:[]
-    };
-    placeNameChangedHandler=event=>{
-        this.setState({placeName:event})//设置状态
-        // alert(event)
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import configureStore from "./src/store/configureStore";
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
-    };
+const store = configureStore();
 
-    placeSubmitHandler=()=>{
-        if(this.state.placeName.trim()===""){
-            return
-        }
-        //trim:remove whitespace from both side
-        this.setState(preState=>{
-            return{
-                places:preState.places.concat(preState.placeName)
-                //concat: join arrays
-            }
-        })
-    };
+// Register Screens
+Navigation.registerComponent(
+    "awesome-places.AuthScreen",
+    () => AuthScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.SharePlaceScreen",
+    () => SharePlaceScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.FindPlaceScreen",
+    () => FindPlaceScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.PlaceDetailScreen",
+    () => PlaceDetailScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.SideDrawer",
+    () => SideDrawer
+);
 
-  render() {
-        const placesOutput=this.state.places.map(
-            (place,i)=>(
-                <ListItem key={i}
-                placeName={place}/>
-            )
-        );//output the array out;
-
-    return (
-      <View style={styles.container}>
-
-          <InputPlace ChangeText={this.placeNameChangedHandler}
-                      Press={this.placeSubmitHandler}/>
-
-
-          <List placesOutput={placesOutput}/>
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-      flex:1,
-      // 默认coclumn方向占满整个屏幕
-      padding:26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',// default columnflex
-      justifyContent: 'flex-start'//move the TextInput to top,main axis
-  }
-
+// Start a App
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: "awesome-places.AuthScreen",
+        title: "Login"
+    }
 });
--- app/src/components/ListItem/ListItem.js
+++ app/src/components/ListItem/ListItem.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
-const listItem =(props)=>(
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+const listItem = props => (
+  <TouchableOpacity onPress={props.onItemPressed}>
     <View style={styles.listItem}>
-        <Text>
-            {props.placeName}
-        </Text>
+      <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} />
+      <Text>{props.placeName}</Text>
     </View>
+  </TouchableOpacity>
 );
-const styles= StyleSheet.create({
-    listItem:{
-        width:"100%",
-        padding:10,
-        margin:10,
-        backgroundColor:"#eee"
-    }
 
-
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  placeImage: {
+      marginRight: 8,
+      height: 30,
+      width: 30
+  }
 });
+
 export default listItem;
