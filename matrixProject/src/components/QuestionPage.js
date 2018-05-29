9d52598c6166a9a6c9d88d0f127deeecda7b3315
GaoYuxin <gaomnemosyne@Gmail.com>
2018-05-29 16:24:46 -0400

    统计分数+随机从题库中选三题显示

--- matrixProject/src/components/QuestionPage.js
+++ matrixProject/src/components/QuestionPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { Card, QuestionCard, AnswerCard, Buttons } from './common';
 import questionData from './questionData.json';
 
 const NUMBER_OF_QUESTIONS = 3;
 
+//shuffle(array) is to randonize an array
 function shuffle(array) {
  for (let i = array.length - 1; i > 0; i--) {
      const j = Math.floor(Math.random() * (i + 1));
@@ -14,10 +15,17 @@ function shuffle(array) {
 
 class QuestionPage extends Component {
 
-  state = { question: [], options: [], correctoption: [], questionIndex: 0 };
+  state = {
+    question: [],
+    options: [],
+    correctoption: [],
+    questionIndex: 0,
+    score: 0,
+    finishAnswer: false
+   };
 
   componentWillMount() {
-     shuffle(questionData.data);
+    shuffle(questionData.data);
     this.setState({ question: questionData.data[0].question,
                     options: questionData.data[0].options,
                     correctoption: questionData.data[0].correctoption,
@@ -26,6 +34,7 @@ class QuestionPage extends Component {
   }
 
   nextButtonOnPress() {
+    this.setState({ finishAnswer: true });
     const c = this.state.questionIndex + 1;
     if (this.state.questionIndex < NUMBER_OF_QUESTIONS - 1) {
       this.setState({ question: questionData.data[c].question,
@@ -33,12 +42,15 @@ class QuestionPage extends Component {
                     correctoption: questionData.data[c].correctoption,
                     questionIndex: c
                   });
+  } else {
+    console.log(this.state.score);
   }
 }
 
-  answerButtonOnPress(status, key) {
+  answerButtonOnPress(key) {
     if (key === this.state.correctoption) {
       console.log('correct!');
+      this.setState({ score: this.state.score + 1 });
     } else {
       console.log('wrong!');
     }
@@ -48,13 +60,16 @@ class QuestionPage extends Component {
     const options = Object.keys(currentOptions).map(key => {
       return (
         <View key={key} style={{ margin: 3 }}>
-         <Buttons onPress={(status) => this.answerButtonOnPress(status, key)}>
+          <TouchableOpacity disabled={this.state.finishAnswer}>
+         <Buttons onPress={() => this.answerButtonOnPress(key)}>
            {currentOptions[key]}
          </Buttons>
+         </TouchableOpacity>
        </View>
      );
    }
  );
+
     return (
       <Card>
         <QuestionCard>
