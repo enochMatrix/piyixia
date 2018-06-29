28474cbd1c69ffac47e8ee6e03f0a2fa4a759877
GaoYuxin <gaomnemosyne@Gmail.com>
2018-06-29 01:50:40 -0400

    add fetch testing for register/login; push changes because 电脑花屏，怕他随时会die。

---
+++ matrixProject/src/LoginPage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { View, TextInput, Text, Button } from 'react-native';
+
+class LoginPage extends Component {
+  state = {
+    username: '',
+    password: '',
+    response: ''
+  }
+// 注册 api
+  tryFetch = async () => {
+  fetch('http://127.0.0.1:3000/register', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+      },
+    body: JSON.stringify({ username: this.state.username, password: this.state.password })
+      })
+    .then((response) => (response.text()))
+    .catch((error) => {
+      console.log(error);
+    })
+    .then((res) => {
+        this.setState({ response: res });
+});
+}
+// 登陆 api
+logIn = async () => {
+fetch('http://127.0.0.1:3000/login', {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
+    },
+  body: JSON.stringify({ username: this.state.username, password: this.state.password })
+    })
+  .then((response) => (response.text()))
+  .catch((error) => {
+    console.log(error);
+  })
+  .then((res) => {
+      this.setState({ response: res });
+      // 登陆成功： change props state and navigation to homepage
+      console.log(res.localeCompare('登陆成功'));
+});
+};
+
+
+  render() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row' }}>
+        <Text>UserName</Text>
+        <TextInput
+           onChangeText={(text) => {
+             this.setState({ username: text });
+           }}
+           style={{ marginLeft: 5, height: 20, width: 100, borderColor: 'gray', borderWidth: 1 }}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', marginTop: 10 }}>
+        <Text>PassWord</Text>
+        <TextInput
+          onChangeText={(text) => {
+            this.setState({ password: text });
+          }}
+           secureTextEntry
+           style={{ marginLeft: 5, height: 20, width: 100, borderColor: 'gray', borderWidth: 1 }}
+        />
+      </View>
+      <Button
+        title='Register'
+        color='black'
+        onPress={this.tryFetch.bind(this)}
+      />
+      <Button
+        title='Log In'
+        color='black'
+        onPress={this.logIn.bind(this)}
+      />
+      <Text style={{ marginTop: 10 }}>{this.state.response}</Text>
+    </View>
+    );
+  }
+}
+
+export default LoginPage;
--- matrixProject/src/components/StartPage.js
+++ matrixProject/src/components/StartPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 class StartPage extends Component {
