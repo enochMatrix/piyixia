a0f2f2396eb14c40b08c788a2fc010e227757216
yinuoyang <yyn4a5d@gmail.com>
2018-10-25 20:41:23 -0400

    no message

---
+++ matrixProject/src/pages/Service/FirebaseManager.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+
+
+export class FirebaseManager {
+
+
+    config = {
+        apiKey: "AIzaSyApicAg8teSzkggqcaGqHnv3T_VFedXZPE",
+        authDomain: "piyixia-562cf.firebaseapp.com",
+        databaseURL: "https://piyixia-562cf.firebaseio.com",
+        projectId: "piyixia-562cf",
+        storageBucket: "piyixia-562cf.appspot.com",
+        messagingSenderId: "157139696379"
+    };
+
+
+    createChallenge = () => {
+
+
+    }
+
+}
